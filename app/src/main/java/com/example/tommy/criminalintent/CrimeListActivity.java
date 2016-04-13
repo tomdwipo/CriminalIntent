000527b3d23f96fe0ccfb95f4888e1bb0164807f
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-13 22:44:52 +0700

    buat model CrimeListActivity.java dan CrimeListFragment.java

---
+++ app/src/main/java/com/example/tommy/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.example.tommy.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tommy on 4/13/16.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/com/example/tommy/criminalintent/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.example.tommy.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tommy on 4/13/16.
+ */
+public class CrimeListFragment extends Fragment {
+}
