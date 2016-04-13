1df16b4299beb34dc5233e00dba697c2435eaac0
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-13 22:56:11 +0700

    Update RecyclerView di CrimeListFragment

--- app/src/main/java/com/example/tommy/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/tommy/criminalintent/CrimeListFragment.java
@@ -1,9 +1,25 @@
 package com.example.tommy.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by tommy on 4/13/16.
  */
 public class CrimeListFragment extends Fragment {
+    private RecyclerView mCrimeRecylerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecylerView = (RecyclerView)view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecylerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
